78de2e084787e24c98b34663320a5204b52bcd22
Pelayo <phuermij@myuax.com>
2022-03-24 08:39:41 +0100

    atributos de la clase

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+#Creamos la clase
+class Libro:
+    
+    #Definimos el constructor con los atributos de la clase
+    def __init__(self, titulo, autor, paginas, isbn):
+        self.titulo = titulo
+        self.autor = autor
+        self.paginas = paginas
+        self.isbn = isbn
+        
