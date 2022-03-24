80ec0a31387318ee87b5128cfdc1e9f4f05d6789
Pelayo <phuermij@myuax.com>
2022-03-24 09:34:47 +0100

    cuenta plazo fijo

--- ejercicio3/Cuentas/plazo_fijo.py
+++ ejercicio3/Cuentas/plazo_fijo.py
@@ -2,4 +2,16 @@ from cuenta import *
 
 #Definimos la clase
 class Plazo_fijo(Cuenta):
-    
+    def __init__(self, fecha_vencimiento):
+        self.fecha_vencimiento = fecha_vencimiento
+
+    def retirar(self):
+        
+        cantidad = int(input("Introduce la cantidad que desea retirar: "))
+
+        if 0 < cantidad < self.saldo:
+            self.saldo = self.saldo - cantidad + (5/100 * cantidad)
+            print("Se ha retirado correctamente")
+
+        else:
+            print("No tiene suficiente dinero")
