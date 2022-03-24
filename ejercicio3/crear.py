a9c02bac730ddafc63ed124a7182167a49d3ec9e
Pelayo <phuermij@myuax.com>
2022-03-24 10:20:04 +0100

    main

--- ejercicio3/crear.py
+++ ejercicio3/crear.py
@@ -1,6 +1,9 @@
+from random import random
 from Cuentas.cuenta import *
 from Cuentas.plazo_fijo import * 
 from Cuentas.vip import *
+import random
+from datetime import datetime
 
 def eleccion_cuenta():
     print("¿Qué tipo de cuenta quieres abrir?:", "\n")
@@ -10,12 +13,30 @@ def eleccion_cuenta():
     n = int(input("Introduce aquí el número para seleccionar: "))
 
     if n == 1:
-        cuenta = Cuenta()
-
+        cuenta = Cuenta() #Creamos el objeto
+        #Datos de la cuenta
+        cuenta.identificador = 12345678
+        cuenta.titular = "Pelayo Huerta Mijares"
+        cuenta.apertura = fecha_aleatoria()
+        cuenta.numero_cuenta = random.randint(10**12, 10**13)
+        cuenta.saldo = 10000
+        
     if n == 2:
-        cuenta = Plazo_fijo()
+        cuenta = Plazo_fijo() #Creamos el objeto
 
     if n == 3:
-        cuenta = Vip()
+        cuenta = Vip() #Creamos el objeto
+
+def fecha_aleatoria():
+
+    inicio = datetime(2017, 1, 30)
+    final =  datetime(2017, 5, 28)
+
+    random_date = inicio + (final - inicio) * random.random()
+
+    return random_date
+
+
+
 
 
---
+++ ejercicio3/main.py
@@ -0,0 +1,8 @@
+from random import random
+from Cuentas.cuenta import *
+from Cuentas.plazo_fijo import * 
+from Cuentas.vip import *
+from crear import *
+
+if __name__ == "__main__":
+    
